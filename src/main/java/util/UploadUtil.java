ed64e355091c2b785eabdf18d4ffc369a3de704b
Hasee <810720094@qq.com>
2020-07-20 10:46:25 +0800

    commit

--- src/main/java/util/UploadUtil.java
+++ src/main/java/util/UploadUtil.java
@@ -7,6 +7,7 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.mime.FormBodyPart;
 import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.ContentBody;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -22,44 +23,40 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class UploadUtil {
-     private static final Logger log = LoggerFactory.getLogger(UploadUtil.class);
+    private static final Logger log = LoggerFactory.getLogger(UploadUtil.class);
 
-
-     public static String postFile(String url,Map<String, Object> param, File file) throws ClientProtocolException, IOException {
-            String res = null;
-            CloseableHttpClient httpClient = HttpClients.createDefault();
-            HttpPost httppost = new HttpPost(url);
-            httppost.setEntity(getMutipartEntry(param,file));
-            CloseableHttpResponse response = httpClient.execute(httppost);
-            HttpEntity entity = response.getEntity();
-            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-                res = EntityUtils.toString(entity, "UTF-8");
-                response.close();
-            } else {
-                res = EntityUtils.toString(entity, "UTF-8");
-                response.close();
-                throw new IllegalArgumentException(res);
-            }
-            return res;
+    public static String postFile(String url, Map<String, Object> param, File file) throws ClientProtocolException,
+            IOException {
+        String res = null;
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost httppost = new HttpPost(url);
+        httppost.setEntity(getMutipartEntry(param, file));
+        CloseableHttpResponse response = httpClient.execute(httppost);
+        HttpEntity entity = response.getEntity();
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            res = EntityUtils.toString(entity, "UTF-8");
+            response.close();
+        } else {
+            res = EntityUtils.toString(entity, "UTF-8");
+            response.close();
+            throw new IllegalArgumentException(res);
         }
+        return res;
+    }
 
-     private static MultipartEntity getMutipartEntry(Map<String, Object> param, File file) throws UnsupportedEncodingException {
-            if (file == null) {
-                throw new IllegalArgumentException("文件不能为空");
-            }
-            FileBody fileBody = new FileBody(file);
-            FormBodyPart filePart = new FormBodyPart("file", fileBody);
-            MultipartEntity multipartEntity = new MultipartEntity();
-            multipartEntity.addPart(filePart);
-
-            Iterator<String> iterator = param.keySet().iterator();
-            while (iterator.hasNext()) {
-                String key = iterator.next();
-                FormBodyPart field = new FormBodyPart(key, new StringBody((String) param.get(key)));
-                multipartEntity.addPart(field);
-
-            }
-            return multipartEntity;
+    private static MultipartEntity getMutipartEntry(Map<String, Object> param, File file) throws UnsupportedEncodingException {
+        if (file == null) {
+            throw new IllegalArgumentException("文件不能为空");
         }
+        FileBody fileBody = new FileBody(file);
+        FormBodyPart filePart = new FormBodyPart("file", fileBody);
+        MultipartEntity multipartEntity = new MultipartEntity();
+        multipartEntity.addPart(filePart);
+        for (Map.Entry<String, Object> entry : param.entrySet()) {
+            FormBodyPart field = new FormBodyPart(entry.getKey(), (ContentBody) entry.getValue());
+            multipartEntity.addPart(field);
+        }
+        return multipartEntity;
+    }
 
 }
